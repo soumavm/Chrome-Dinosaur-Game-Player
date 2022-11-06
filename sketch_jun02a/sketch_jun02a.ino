3109aef6db7046417715cf7a30d39fd6510f7f52
Soumav <soumavm@gmail.com>
2022-11-06 17:11:27 -0500

    Added Colour reverse logic

--- sketch_jun02a/sketch_jun02a.ino
+++ sketch_jun02a/sketch_jun02a.ino
@@ -1,10 +1,11 @@
 #include <ESP32Servo.h>
 
 const int lowerPin = 26, upperPin = 27;
-const int lowerDarkBound = 3500, lowerLightBound = 4000, 
+const int lowerDarkBound = 3000, lowerLightBound = 3400, 
           upperDarkBound = 2800, upperLightBound = 3100;
 // variable for storing the potentiometer value
 int lowerResValue, upperResValue;
+bool bgWhite = true;
 
 Servo servo;
 const byte servoGPIO = 25;
@@ -27,13 +28,30 @@ void loop() {
 
   Serial.print("Upper Val: ");
   Serial.println(upperResValue);
-
-  if(lowerResValue < 3900){
-    Serial.println("HERE HERE HERE HERE ");
-   }
-  if(lowerResValue < lowerDarkBound || upperResValue < upperDarkBound){
-    servo.write(15);
-    delay(150);
-    servo.write(0); 
+  if(bgWhite){
+    if(lowerResValue < lowerDarkBound || upperResValue < upperDarkBound){
+      servo.write(15);
+      delay(150);
+      servo.write(0); 
+    }
+    if(upperResValue < upperDarkBound){
+      delay(500);
+      if(upperResValue < upperDarkBound){
+        bgWhite = false;
+      }
+    }
+  }
+  else{
+    if(lowerResValue < lowerLightBound || upperResValue < upperLightBound){
+      servo.write(15);
+      delay(200);
+      servo.write(0); 
+    }
+    if(upperResValue < upperLightBound){
+      delay(500);
+      if(upperResValue < upperLightBound){
+        bgWhite = true;
+      }
+    }
   }
 }
