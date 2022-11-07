33b80c805cc71c5d2873402d43cc13aa753f50aa
Soumav <soumavm@gmail.com>
2022-11-06 20:28:41 -0500

    Added comments, cleaned up code

--- sketch_jun02a/sketch_jun02a.ino
+++ sketch_jun02a/sketch_jun02a.ino
@@ -1,26 +1,30 @@
 #include <ESP32Servo.h>
 
+//sets Voltage sensing pins, initializes voltage values for object tracking
 const int lowerPin = 26, upperPin = 27;
 const int lowerBound = 3100, lowerInvertBound = 1700, 
           upperBound = 2900, upperInvertBound = 1780;
-// variable for storing the potentiometer value
-int lowerResValue, upperResValue;
+          
+// Stores the reference voltages at each voltage divider
+int lowerResValue=0, upperResValue=0;
+
+//used to detect if screen in inverted or not
 bool bgWhite = true;
 int delayCount=0;
 
+//sets up which pin controls SG90 servo
 Servo servo;
 const byte servoGPIO = 25;
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  
+  //Serial.begin(115200);
+  //Ataches servo to pin
   servo.attach(servoGPIO);
   servo.write(0);
 }
 
 void loop() {
-  // Reading potentiometer value
+  // Reading voltage divider voltages
   lowerResValue = analogRead(lowerPin);
   upperResValue = analogRead(upperPin);
 
@@ -36,11 +40,13 @@ void loop() {
   
   if(bgWhite){
     if(lowerResValue < lowerBound || upperResValue < upperBound){
+      //simulates jump
       servo.write(7);
       delay(150);
       servo.write(0);
 
     }
+    //checks to see if the upper photoresistor is triggerred off of an object, or the screen switching colours
     if((lowerResValue < lowerBound) && (upperResValue < upperBound)){
       delayCount+=2;
     }
